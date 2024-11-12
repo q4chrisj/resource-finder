42af34b3831cc6f6b28b32bffa8eddaf8e320065
Chris Jones <chrisj@q4inc.com>
2024-11-12 14:50:48 -0500

    chore: additional things

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { promises as fs } from "fs";
 import os from "os";
 import * as fsWalk from "@nodelib/fs.walk";
-import { TFState } from "./TFState";
+import { TFState } from "./tf_state";
 
-const resources: string[] = [
+const resources_to_locate: string[] = [
   "aws_subnet",
   "aws_default_network_acl",
   "aws_network_acl",
@@ -20,11 +20,8 @@ const found: Map<string, string[]> = new Map();
 async function run(): Promise<void> {
   const path_to_states =
     os.homedir() + "/Projects/_todo/micro-dev-states/environment/";
-  // const path_to_states =
-  //   os.homedir() +
-  //   "/Projects/_todo/micro-dev-states/environment/sbr-000/com/q4inc/connect/platform/scheduler/eds/";
 
-  const results = fsWalk.walk(path_to_states, async (error, entry) => {
+  fsWalk.walk(path_to_states, async (error, entry) => {
     if (error) {
       console.error(error);
       return;
@@ -32,35 +29,29 @@ async function run(): Promise<void> {
 
     entry.forEach(async (entry) => {
       if (!entry.dirent.isDirectory()) {
-        // console.log(`Entry: ${entry.path}`);
         const state_contents = await fs.readFile(entry.path, "utf8");
         const state_data: TFState = JSON.parse(state_contents);
 
         if (state_data.resources.length > 0) {
           state_data.resources.forEach((resource) => {
             if (resource.mode === "managed") {
-              // console.log(`Found a managed resource: ${resource.type}`);
-              // console.log(
-              //   `Resource contains ${resource.instances.length} instances`,
-              // );
-              //
-              // console.log(`Resource type ${resource.type} is in the list`);
-              resource.instances.forEach((instance) => {
-                if (instance.attributes.arn) {
-                  console.log(`Instance ARN: ${instance.attributes.arn}`);
-                  if (found.has(resource.type)) {
-                    let current = found.get(resource.type);
-                    if (current) {
-                      current.push(instance.attributes.arn);
+              if (resources_to_locate.includes(resource.type)) {
+                resource.instances.forEach((instance) => {
+                  if (instance.attributes.arn) {
+                    if (found.has(resource.type)) {
+                      let current = found.get(resource.type);
+                      if (current) {
+                        current.push(instance.attributes.arn);
+                      } else {
+                        current = [instance.attributes.arn];
+                      }
+                      found.set(resource.type, current);
                     } else {
-                      current = [instance.attributes.arn];
+                      found.set(resource.type, [instance.attributes.arn]);
                     }
-                    found.set(resource.type, current);
-                  } else {
-                    found.set(resource.type, [instance.attributes.arn]);
                   }
-                }
-              });
+                });
+              }
             }
           });
         }
